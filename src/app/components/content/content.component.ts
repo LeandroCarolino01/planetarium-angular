a80cd7d7867055a8014b1e0a320c2fe334cf8b8d
Leandro Carolino Dos Santos <leandro@watermelon.co>
2019-08-14 15:27:25 +0200

    navbar added

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Curiosity } from '../../../Curiosity';
 
 @Component({
   selector: 'app-content',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-
+  curiosities:Curiosity[];
   constructor() { }
 
   ngOnInit() {
+    this.curiosities =  [
+      {
+        id:1,
+        name:'first',
+        img:'assets/images/img1.jpg',
+        description: 'another test'
+      },
+      {
+        id:2,
+        name:'first',
+        img:'assets/images/img2.jpg',
+        description: 'another test'
+      },
+      {
+        id:3,
+        name:'first',
+        img:'assets/images/img3.jpg',
+        description: 'another test'
+      }
+    ]
   }
 
 }
