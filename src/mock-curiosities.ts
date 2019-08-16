fcae970a79636f8c6ecc676fe237bed1a56b9193
Leandro Carolino Dos Santos <leandro@watermelon.co>
2019-08-16 11:24:34 +0200

    all objects added to ngFor

--- src/mock-curiosities.ts
+++ src/mock-curiosities.ts
@@ -18,5 +18,35 @@ export const CURIOSITIES: Curiosity[] = [
       name:'first',
       img:'assets/images/img3.jpg',
       description: 'another test'
-    }
+    },
+    {
+        id:4,
+        name:'first',
+        img:'assets/images/img4.jpg',
+        description: 'another test'
+      },
+      {
+        id:5,
+        name:'first',
+        img:'assets/images/img5.jpg',
+        description: 'another test'
+      },
+      {
+        id:6,
+        name:'first',
+        img:'assets/images/img6.jpg',
+        description: 'another test'
+      },
+      {
+        id:7,
+        name:'first',
+        img:'assets/images/img7.jpg',
+        description: 'another test'
+      },
+      {
+        id:8,
+        name:'first',
+        img:'assets/images/img8.jpg',
+        description: 'another test'
+      }
   ]
