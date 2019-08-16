de999d5fba0ae40f10160d64c5eec9b699190037
Leandro Carolino Dos Santos <leandro@watermelon.co>
2019-08-16 11:32:09 +0200

    navbar ficed

--- src/mock-curiosities.ts
+++ src/mock-curiosities.ts
@@ -2,22 +2,22 @@ import { Curiosity } from './Curiosity';
 
 export const CURIOSITIES: Curiosity[] = [
     {
-      id:1,
-      name:'first',
-      img:'assets/images/img1.jpg',
-      description: 'another test'
+        id:1,
+        name:'first',
+        img:'assets/images/img1.jpg',
+        description: 'another test'
     },
     {
-      id:2,
-      name:'first',
-      img:'assets/images/img2.jpg',
-      description: 'another test'
+        id:2,
+        name:'first',
+        img:'assets/images/img2.jpg',
+        description: 'another test'
     },
     {
-      id:3,
-      name:'first',
-      img:'assets/images/img3.jpg',
-      description: 'another test'
+        id:3,
+        name:'first',
+        img:'assets/images/img3.jpg',
+        description: 'another test'
     },
     {
         id:4,
