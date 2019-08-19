3d53df70ac9e55602d3ecbb76b676dab7602c702
Leandro Carolino Dos Santos <leandro@watermelon.co>
2019-08-19 16:28:46 +0200

    mock data modified

--- src/mock-curiosities.ts
+++ src/mock-curiosities.ts
@@ -5,13 +5,13 @@ export const CURIOSITIES: Curiosity[] = [
         id:1,
         name:'first',
         img:'assets/images/img1.jpg',
-        description: 'another test'
+        description: 'Jupiter was appropriately named after the king of the gods. It’s massive, has a powerful magnetic field, and more moons that any planet in the Solar System. Though it has been known to astronomers since ancient times, the invention of the telescope and the advent of modern astronomy has taught us so much about this gas giant.'
     },
     {
         id:2,
         name:'first',
         img:'assets/images/img2.jpg',
-        description: 'another test'
+        description: 'Astronomers call Jupiter a failed star, but that’s not really an appropriate description. While it is true that, like a star, Jupiter is rich in hydrogen and helium, Jupiter does not have nearly enough mass to trigger a fusion reaction in its core. This is how stars generate energy, by fusing hydrogen atoms together under extreme heat and pressure to create helium, releasing light and heat in the process.'
     },
     {
         id:3,
