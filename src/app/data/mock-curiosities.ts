2786345cad44731eec85414baf8b06a33f1f745a
leandro <carolino.leandro@gmail.com>
2020-07-06 14:44:47 +0200

    bugfix(json changed)

--- src/app/data/mock-curiosities.ts
+++ src/app/data/mock-curiosities.ts
@@ -3,27 +3,27 @@ import { Curiosity } from '../../models/Curiosity';
 export const CURIOSITIES: Curiosity[] = [
     {
         id:1,
-        name:'first',
+        name:'Why is it called Jupiter',
         img:'assets/images/img1.jpg',
         description: 'Jupiter was appropriately named after the king of the gods. It’s massive, has a powerful magnetic field, and more moons that any planet in the Solar System. Though it has been known to astronomers since ancient times, the invention of the telescope and the advent of modern astronomy has taught us so much about this gas giant.'
     },
     {
         id:2,
-        name:'first',
+        name:'2. Jupiter Cannot Become A Star:',
         img:'assets/images/img2.jpg',
-        description: 'test'
+        description: 'Astronomers call Jupiter a failed star, but that’s not really an appropriate description. While it is true that, like a star, Jupiter is rich in hydrogen and helium, Jupiter does not have nearly enough mass to trigger a fusion reaction in its core. This is how stars generate energy, by fusing hydrogen atoms together under extreme heat and pressure to create helium, releasing light and heat in the process. This is made possible by their enormous gravity. For Jupiter to ignite a nuclear fusion process and become a star, it would need more than 70 times its current mass. If you could crash dozens of Jupiters together, you might have a chance to make a new star. But in the meantime, Jupiter shall remain a large gas giant with no hopes of becoming a star. Sorry, Jupiter!'
     },
     {
         id:3,
-        name:'first',
+        name:'(the red spot) -  The Great Red Spot Has Been Around For A Long Time:',
         img:'assets/images/img3.jpg',
-        description: 'Astronomers call Jupiter a failed star, but that’s not really an appropriate description. While it is true that, like a star, Jupiter is rich in hydrogen and helium, Jupiter does not have nearly enough mass to trigger a fusion reaction in its core. This is how stars generate energy, by fusing hydrogen atoms together under extreme heat and pressure to create helium, releasing light and heat in the process.'
+        description: 'The Great Red Spot on Jupiter is one of its most familiar features. This persistent anticyclonic storm, which is located south of its equator, measures between 24,000 km in diameter and 12–14,000 km in height. As such, it is large enough to contain two or three planets the size of Earth’s diameter. And the spot has been around for at least 350 years, since it was spotted as far back as the 17th century. The Great Red Spot was first identified in 1665 by Italian astronomer Giovanni Cassini. By the 20th century, astronomers began to theorize that it was a storm, one which was created by Jupiter’s turbulent and fast-moving atmosphere. These theories were confirmed by the Voyager 1 mission, which observed the Giant Red Spot up close in March of 1979 during its flyby of the planet. However, it appears to have been shrinking since that time. Based on Cassini’s observations, the size was estimated to be 40,000 km in the 17th century, which was almost twice as large as it is now. Astronomers do not know if or when it will ever disappear entirely, but they are relatively sure that another one will emerge somewhere else on the planet.'
     },
     {
         id:4,
-        name:'first',
+        name:'( it has 67 moons)  Jupiter Has 67 Moons:',
         img:'assets/images/img4.jpg',
-        description: 'Raw Sockets − These provide users access to the underlying communication protocols, which support socket abstractions. These sockets are normally datagram oriented, though their exact characteristics are dependent on the interface provided by the protocol. Raw sockets are not intended for the general user; they have been provided mainly for those interested in developing new communication protocols, or for gaining access to some of the more cryptic facilities of an existing protocolequenced Packet Sockets − They are similar to a stream socket, with the exception that record boundaries are preserved. This interface is provided only as a part of the Network Systems (NS) socket abstraction, and is very important in most serious NS applications. Sequenced-packet sockets allow the user to manipulate the Sequence Packet Protocol (SPP) or Internet Datagram Protocol (IDP) headers on a packet or a group of packets, either by writing a prototype header along with whatever data is to be sent, or by specifying a default header to be used with all outgoing data, and allows the user to receive the headers on incoming packets.'
+        description: 'As of the penning of this article, Jupiter has a 67 confirmed and named satellites. However, it is estimated that the planet has over 200 natural satellites orbiting it. Almost all of them are less than 10 kilometers in diameter, and were only discovered after 1975, when the first spacecraft (Pioneer 10) arrived at Jupiter. However, it also has four major moons, which are collectively known as the Galilean Moons (after their discovered Galileo Galilei). These are, in order of distance from Jupiter, Io, Europa, Ganymede, and Callisto. These moons are some of the largest in the Solar System, with Ganymede being the largest, measuring 5262 km in diameter.'
       },
       {
         id:5,
